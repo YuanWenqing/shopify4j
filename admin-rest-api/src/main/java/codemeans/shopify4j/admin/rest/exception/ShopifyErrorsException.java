cebb2b7a2cab8886c0def6948cdc15609178a833
yuan <yuan@preferr.com>
2021-01-12 16:59:48 +0800

    shopify error

---
+++ admin-rest-api/src/main/java/codemeans/shopify4j/admin/rest/exception/ShopifyErrorsException.java
@@ -0,0 +1,18 @@
+package codemeans.shopify4j.admin.rest.exception;
+
+import codemeans.shopify4j.admin.rest.model.errors.ShopifyError;
+import com.google.common.collect.Lists;
+import java.util.List;
+
+/**
+ * exception because of shopify error code
+ *
+ * @author: yuanwq
+ * @date: 2021-01-12
+ */
+public class ShopifyErrorsException {
+
+  private final List<ShopifyError> errors = Lists.newArrayList();
+
+
+}
