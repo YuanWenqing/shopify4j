8bdd7152b9512676859971910abfc07c89b11cf0
yuan <yuan@preferr.com>
2021-01-19 18:02:16 +0800

    HttpResponse

---
+++ shopify4j-core/src/main/java/codemeans/shopify4j/core/http/HttpResponse.java
@@ -0,0 +1,54 @@
+package codemeans.shopify4j.core.http;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/**
+ * Generic Http Response
+ *
+ * @author: yuanwq
+ * @date: 2021-01-19
+ */
+public class HttpResponse<T> {
+
+  private final String body;
+  private final T data;
+  private final Map<String, List<String>> headers = new LinkedHashMap<>();
+
+  public HttpResponse(String body, T data) {
+    this.body = body;
+    this.data = data;
+  }
+
+  public HttpResponse<T> addAllHeaders(Map<String, List<String>> headers) {
+    for (Entry<String, List<String>> entry : headers.entrySet()) {
+      this.headers.computeIfAbsent(entry.getKey().toLowerCase(), s -> new ArrayList<>())
+          .addAll(entry.getValue());
+    }
+    return this;
+  }
+
+  /**
+   * last value for specific header
+   *
+   * @param header case-ignore
+   */
+  public String getHeader(String header) {
+    List<String> values = getHeaders(header);
+    return values.isEmpty() ? null : values.get(values.size() - 1);
+  }
+
+  /**
+   * all values for specific header
+   *
+   * @param header case-ignore
+   * @return empty list if no header exists
+   */
+  public List<String> getHeaders(String header) {
+    return headers.getOrDefault(header.toLowerCase(), Collections.emptyList());
+  }
+}
