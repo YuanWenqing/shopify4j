b433fe64e9e27e3e8d664416798aabd7b98d863b
yuan <yuan@preferr.com>
2021-01-26 11:55:53 +0800

    ContextForTest

---
+++ graphql-admin-api/src/test/java/codemeans/shopify4j/graphql/admin/ContextForTest.java
@@ -0,0 +1,44 @@
+package codemeans.shopify4j.graphql.admin;
+
+import codemeans.shopify4j.core.store.MemoryStoreSettingStorage;
+import codemeans.shopify4j.core.store.StoreSetting;
+import codemeans.shopify4j.graphql.admin.sdk.DefaultGraphqlStore;
+import codemeans.shopify4j.graphql.admin.sdk.GraphqlStore;
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+import org.apache.commons.io.IOUtils;
+
+/**
+ * @author: yuanwq
+ * @date: 2021-01-13
+ */
+public class ContextForTest {
+
+  public static final StoreSetting STORE_SETTING = loadStoreSetting("store.properties");
+  public static final MemoryStoreSettingStorage STORE_SETTING_STORAGE = new MemoryStoreSettingStorage();
+
+  static {
+    STORE_SETTING_STORAGE.registerStore(STORE_SETTING);
+  }
+
+  public static final GraphqlStore TEST_STORE = new DefaultGraphqlStore(STORE_SETTING);
+
+  private static StoreSetting loadStoreSetting(String resourceName) {
+    try {
+      StoreSetting setting = new StoreSetting();
+      Properties properties = new Properties();
+      properties.load(new StringReader(IOUtils.resourceToString(resourceName,
+          StandardCharsets.UTF_8, ContextForTest.class.getClassLoader())));
+      setting.setStoreDomain(properties.getProperty("store-domain"));
+      setting.setApiKey(properties.getProperty("api-key"));
+      setting.setApiPassword(properties.getProperty("api-password"));
+      setting.setApiVersion(properties.getProperty("api-version", setting.getApiVersion()));
+      return setting;
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+  }
+}
