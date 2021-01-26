64d94bfaaab637be391edc400a2fd8cf6398c0ba
yuan <yuan@preferr.com>
2021-01-26 15:24:38 +0800

    testcase

--- graphql-admin-api/src/test/java/codemeans/shopify4j/graphql/admin/products/TestProduct.java
+++ graphql-admin-api/src/test/java/codemeans/shopify4j/graphql/admin/products/TestProduct.java
@@ -1,32 +1,32 @@
-package codemeans.shopify4j.graphql.admin.stores;
+package codemeans.shopify4j.graphql.admin.products;
 
 import static codemeans.shopify4j.graphql.admin.ContextForTest.TEST_STORE;
 
 import codemeans.shopify4j.graphql.admin.exception.GraphqlApiException;
 import codemeans.shopify4j.graphql.admin.types.Operations;
+import codemeans.shopify4j.graphql.admin.types.ProductConnection;
 import codemeans.shopify4j.graphql.admin.types.QueryResponse;
 import codemeans.shopify4j.graphql.admin.types.QueryRootQuery;
-import codemeans.shopify4j.graphql.admin.types.Shop;
 import org.junit.Test;
 
 /**
  * @author: yuanwq
  * @date: 2021-01-26
  */
-public class TestShop {
+public class TestProduct {
 
   @Test
-  public void testGetShop() throws GraphqlApiException {
+  public void testListProducts() throws GraphqlApiException {
     QueryRootQuery queryRootQuery = Operations.query(query ->
-        query.shop(shop ->
-            shop.name()
-                .myshopifyDomain()
-                .primaryDomain(domain -> domain.host().sslEnabled().url())
-                .email()
-                .contactEmail()));
+        query.products(args -> args.first(10),
+            products -> products
+                .edges(edges -> edges.node(node -> node.title()))
+                .pageInfo(pageInfo -> pageInfo.hasNextPage()))
+    );
     QueryResponse queryResponse = TEST_STORE.query(queryRootQuery);
     System.out.println(queryResponse.prettyPrintJson());
-    Shop data = queryResponse.getData().getShop();
+    ProductConnection data = queryResponse.getData().getProducts();
     System.out.println(data.responseData);
+
   }
 }
