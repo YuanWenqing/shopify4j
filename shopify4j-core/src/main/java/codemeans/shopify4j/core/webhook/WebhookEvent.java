bc39ae94208fe3cc4727c96571a3f16c5952f776
yuan <yuan@preferr.com>
2021-02-01 15:41:30 +0800

    webhook event

---
+++ shopify4j-core/src/main/java/codemeans/shopify4j/core/webhook/WebhookEvent.java
@@ -0,0 +1,29 @@
+package codemeans.shopify4j.core.webhook;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * a wrapper class for whole data of event
+ *
+ * @author: yuanwq
+ * @date: 2021-02-01
+ */
+@Data
+@Accessors(chain = true)
+public class WebhookEvent<D> {
+
+  private String topic;
+  /**
+   * sha256
+   */
+  private String hmac;
+  private String shopDomain;
+  private String apiVersion;
+  private String webhookId;
+
+  /**
+   * event data
+   */
+  private D data;
+}
